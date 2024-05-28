e1f1c2563f1da6d18eeff1cae170915dae2c3793
parth5757 <psthakkar2@gmail.com>
2024-05-28 09:55:37 +0530

    basic python code updated

---
+++ basic_code/patterns/square.py
@@ -0,0 +1,20 @@
+size = 5
+
+# create a list of rows
+
+# for i in range(0, size):
+#     # Create a list of columns
+#     for j in range(0, size):
+#         print("*", end="")
+#     print()
+
+
+
+
+# print(i)
+# print(j)
+
+# 2 method
+
+for i in range(0, size):
+    print("*" * size)
