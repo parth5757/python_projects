49e14070589482f05379adbd20c828acb1cacb55
parth5757 <psthakkar2@gmail.com>
2024-09-22 17:37:10 +0530

    pytest example fixture work done

--- pytest/unit_testing_example/test_shopping_cart.py
+++ pytest/unit_testing_example/test_shopping_cart.py
@@ -1,29 +1,27 @@
 from shopping_cart import ShoppingCart
 import pytest
 
+@pytest.fixture
+def cart():
+    return ShoppingCart(5)
 
-
-def test_can_add_item_to_cart():
-    cart = ShoppingCart(5)
+def test_can_add_item_to_cart(cart):
     cart.add("apple")
     assert cart.size() == 1
 
 # def test_item_added_cart_contain_items():
 
-def test_when_item_added_cart_contain_item():
-    cart = ShoppingCart(5)
+def test_when_item_added_cart_contain_item(cart):
     cart.add("apple")
     assert "apple" in cart.get_items()
 
-def test_when_more_than_max_item_than_failed():
-    cart = ShoppingCart(5)
+def test_when_more_than_max_item_than_failed(cart):
     for _ in range(5):
         cart.add("apple")
     with pytest.raises(OverflowError):
         cart.add("apple")
 
-def test_can_get_total_price():
-    cart = ShoppingCart(5)
+def test_can_get_total_price(cart):
     cart.add("Mung")
     cart.add("Rajma")
 
