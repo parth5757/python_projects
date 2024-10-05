17962e620350e89508d7757c7d227e3056b84749
parth5757 <psthakkar2@gmail.com>
2024-10-05 22:42:53 +0530

    odd even & angram buuble sor added

--- basic_code/common_asked_code/anagram_check.py
+++ basic_code/common_asked_code/anagram_check.py
@@ -1,8 +1,20 @@
-# શબ્દને આડાઅવળા ગોઠવીને બનાવેલો નવો શબ્દ, વાક્યોને આડાઅવળા ગોઠવીને બનાવેલા નવા વાક્ય
-
 def are_anagrams(str1, str2):
-    # print(sorted(str1), sorted(str2))
-    return sorted(str1) == sorted(str2)
+    
+    def bubble_sort(str):
+        for i in range(0, len(str)):
+            for j in range(i+1, len(str)):
+                if str[j] < str[i]:
+                    temp = str[j]    
+                    str[j] = str[i]    
+                    str[i] = temp    
+        return str
+
+    sorted_str1 = bubble_sort(list(str1))
+    print(sorted_str1)
+    sorted_str2 = sorted(str2)
+    print(sorted_str2)
+
+    return sorted_str1 == sorted_str2
 # Test the function
 print(are_anagrams("listen", "silent"))  # Output: True
 print(are_anagrams("hello", "world"))    # Output: False
