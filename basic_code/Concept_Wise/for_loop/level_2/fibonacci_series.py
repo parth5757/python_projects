42439d0d042138a7eba36ddc48c8f4a9a1508862
Parth Thakkar <psthakkar2@gmail.com>
2025-04-05 06:44:38 +0530

    fibonacci series with working other dictnoary comprehension method.

--- basic_code/Concept_Wise/for_loop/level_2/fibonacci_series.py
+++ basic_code/Concept_Wise/for_loop/level_2/fibonacci_series.py
@@ -14,10 +14,17 @@ print(b)
 
 # method 2
 print("method 2")
-fib_dict = {0:1, 1:1}
-[ans := fib_dict.update({0: fib_dict[1], 1:fib_dict[0]+fib_dict[1]}) for i in range(n)]
-# for i in range(n):
-#     fib_dict.update({0: fib_dict[1], 1:fib_dict[0]+fib_dict[1]})
+# fib_dict = {0:1, 1:1}
+# [ans := fib_dict.update({0: fib_dict[1], 1:fib_dict[0]+fib_dict[1]}) for i in range(n)]
+# print(ans) #my wrong logic
 
-# print(fib_dict[0])
-print(ans)
+fib_dict = {0: 1, 1: 1}
+fib_list = [fib_dict[1]] + [fib_dict.setdefault(i, fib_dict[i-1] + fib_dict[i-2]) for i in range(2, n+1)] # chatgpt
+print(fib_list)
+
+# method 3
+print("method 3")
+for i in range(n):
+    fib_dict.update({0: fib_dict[1], 1:fib_dict[0]+fib_dict[1]}) #my working logic
+print(fib_dict[0])
+
