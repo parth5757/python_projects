fca9eda740468946aefb1ad4153f74572bac2ac2
Parth Thakkar <psthakkar2@gmail.com>
2025-04-09 22:35:09 +0530

    largest number find with the for loop enumerate, reduce and custom sorting methods.

--- basic_code/Concept_Wise/for_loop/level_2/largest_number_from_list.py
+++ basic_code/Concept_Wise/for_loop/level_2/largest_number_from_list.py
@@ -31,3 +31,42 @@ print(max(number))
 print("method 4")
 largest_number = sorted(number, reverse=True)
 print(largest_number[0])
+
+# method 5
+print("method 5")
+# Bubble Sort
+def bubble_sort(s):
+    s = list(s)
+    for i in range(len(s)):
+        for j in range(len(s) - i - 1):
+            if s[j] > s[j + 1]:
+                s[j], s[j + 1] = s[j + 1], s[j]
+    return s[-1]
+# Quick Sort
+def quick_sort(s):
+    s = list(s)
+    for i in range(len(s)):
+        min_idx = i
+        for j in range(i + 1, len(s)):
+            if s[j] < s[min_idx]:
+                min_idx = j
+        s[i], s[min_idx] = s[min_idx], s[i]
+    return s[-1]
+largest_number = bubble_sort(number), quick_sort(number)
+print(largest_number)
+
+
+# method 6 # enumerate use but not make scene to use just time and space more consuming.
+print("method 6")
+largest_number = number[0]
+for idx, val in enumerate(number):
+    if val > largest_number:
+        largest_number = val
+print(largest_number)
+
+# method 7
+print("method 7")
+from functools import reduce
+largest_number = reduce(lambda x, y: x if x>y else y, number)
+print(largest_number)
+    
