c60ed23e475ea0cd75f1cf9b1cfcba9d71cc4f0e
Parth Thakkar <psthakkar2@gmail.com>
2025-04-14 16:49:39 +0530

    added swapping variable with 3 different methods.

--- basic_code/common_asked_code/swapping_variables.py
+++ basic_code/common_asked_code/swapping_variables.py
@@ -1,7 +1,8 @@
 # Python program to swap two variables
-
-x = 5
-y = 10
+# method 1
+print("method 1")
+x = "parth"
+y = "krishna"
 
 # To take inputs from the user
 #x = input('Enter value of x: ')
@@ -14,3 +15,32 @@ y = temp
 
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
+
+# method 2 # this method is only for the int value variable only with using any 3rd variable.
+print("method 2")
+x, y = 10, 50
+
+x = x + y
+y = x - y
+x = x - y
+
+print(f"updated x = {x} & y = {y}")
+
+# method 3
+print("method 3")
+x, y = "Ram", "Sita"
+
+x, y = y, x
+
+print(f"updated x = {x} & y = {y}")
+
+# method 4 # using xor(^)(it only use when the variable value data type is integer).
+print("method 4")
+x, y = 10, 50
+
+x = x ^ y  
+y = x ^ y  
+x = x ^ y  
+
+print("x:", x)
+print("y:", y)
