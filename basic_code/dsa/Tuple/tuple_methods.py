3988ce6044639731cf63183e6a5b2bbf0b9d8dc6
Parth Thakkar <psthakkar2@gmail.com>
2025-05-25 13:42:43 +0530

    tuple method practice code.

---
+++ basic_code/dsa/Tuple/tuple_methods.py
@@ -0,0 +1,12 @@
+# Creating a tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Accessing elements
+print(my_tuple[0])  # Output: 1
+print(my_tuple[-1])  # Output: 5
+
+# Slicing
+print(my_tuple[1:3])  # Output: (2, 3)
+
+# count any element
+print(my_tuple.count(2))
