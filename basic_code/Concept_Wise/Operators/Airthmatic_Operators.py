a478e830bed252f183226f68b80c1fcb5964c1b2
parth5757 <psthakkar2@gmail.com>
2025-10-16 10:02:55 +0530

    Airthmatic operators code added.

---
+++ basic_code/Concept_Wise/Operators/Airthmatic_Operators.py
@@ -0,0 +1,12 @@
+a = 15
+b = 4
+
+print("Addition", a+b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division", a // b)
+print("Modulus", a % b)
+# a = 3
+# b = 3
+print("Exponent", a ** b)
