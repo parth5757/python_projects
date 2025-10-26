e37d33e8692910fd299c6e8bda8ae0be1082ed47
parth5757 <psthakkar2@gmail.com>
2025-10-26 10:40:38 +0530

    basic stack push pop operation.

---
+++ basic_code/dsa/stack/stack.py
@@ -0,0 +1,18 @@
+stack = []
+# append work as push
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('Initial Stack')
+print(stack)
+
+print('\nElement popped from stack: ')
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\n stack element after all element are poped: ')
+
+print(stack)
