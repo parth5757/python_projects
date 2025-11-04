80ffb3eee51823a19e680b7c5a35956493b2bdd9
parth5757 <psthakkar2@gmail.com>
2025-11-04 13:04:27 +0530

    BackSlash Operator different usages.

--- basic_code/Concept_Wise/Operators/Airthmatic_Operators.py
+++ basic_code/Concept_Wise/Operators/Airthmatic_Operators.py
@@ -10,3 +10,35 @@ print("Modulus", a % b)
 # a = 3
 # b = 3
 print("Exponent", a ** b)
+
+# BackSlash Usages
+
+result = 10 * 5 + 2 * 3
+print(result)
+# result = 10 * 5 + 2 * 3 - # it give syntax error (Invalid Syntax)
+result = 10 * 5 + 2 * 3 - \
+        8 
+print(result)
+result = 10 * 5 + 2 * 3 - \
+        8 + 7 * 6
+print(result)
+
+
+# Airthmatic same type of operator with another example.
+if 10 > 5 and \
+    20 > 15 and \
+    30 > 25:
+    print("All condition True")
+
+# same thing written as this following way also.
+if 10 > 5 and 20 > 15 and  30 > 25:
+    print("Same this way All condition True")
+
+# It can be use with the list
+numbers = [1, 2, 3, 4, \
+           5, 6, 7, 8]
+print(numbers)
+
+# Use with print statment also.
+print("Python allows breaking long lines \
+for better readability.")
