27e0fe6086577aa43b3b6aec7ead5730d07017a2
parth5757 <psthakkar2@gmail.com>
2026-01-21 11:17:34 +0530

    Remove leetcode folder from repository

--- leetcode/python/58.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        p = s.split()
-        l = p[-1]
-        len(l)
-        return len(l)
-m = Solution
-s = "Hello moon"
-print(m.lengthOfLastWord(m,s))
